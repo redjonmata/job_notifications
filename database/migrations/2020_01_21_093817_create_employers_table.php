18cf7f50560fa2509ac09d1b52b861acb565df22
Redjon Mata <redjon1997@gmail.com>
2020-02-04 21:57:15 +0100

    Added image column to employers table

--- database/migrations/2020_01_21_093817_create_employers_table.php
+++ database/migrations/2020_01_21_093817_create_employers_table.php
@@ -26,6 +26,7 @@ class CreateEmployersTable extends Migration
             $table->string('mobile_phone',100);
             $table->string('field',100);
             $table->string('specialized_field',100);
+            $table->string('image',100);
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
             $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP'));
         });
